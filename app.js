cf2790f2f44967739c6e998955a061b13b55bedf
Thomas <lesaux.thomas@gmail.com>
2020-10-23 17:23:39 +0200

    resquest ok

--- app.js
+++ app.js
@@ -3,11 +3,115 @@ const db = require("./db");
 
 const app = express();
 
-app.get('/api/v1/todos', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'todos retrieved successfully',
-        todos: db
+app.get('/getRestaurant/:id', async (req, res, next) => {
+    return new Promise((resolve, reject) => {
+        resolve(db.getRestaurantById(req.params.id))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
+    })
+});
+
+app.post('/newRestaurant', async (req, res) => {
+    return new Promise((resolve, reject) => {
+        resolve(db.createRestaurant(req.query))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
+    })
+});
+
+app.put('/restaurants/:restaurant_id', async (req, res) => {
+    return new Promise((resolve, reject) => {
+        resolve(db.updateRestaurant(req.params.restaurant_id, req.query))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
+    })
+});
+
+app.delete('/restaurants/:restaurant_id', async (req, res) => {
+    return new Promise((resolve, reject) => {
+        resolve(db.deleteRestaurant(req.params.restaurant_id))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
+    })
+});
+
+app.get('/restaurants_price_average', async (req, res) => {
+    return new Promise((resolve, reject) => {
+        resolve(db.getAveragePrice())
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
+    })
+});
+
+app.get('/restaurants/average_rating', async (req, res) => {
+    return new Promise((resolve, reject) => {
+        resolve(db.getRestaurantRating())
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
+    })
+});
+
+app.get('/restaurants', async (req, res) => {
+    return new Promise((resolve, reject) => {
+        resolve(db.findRestaurantsWithLocation(req.query.long_coordinates, req.query.lat_coordinates, req.query.max_distance))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
     })
 });
 
--- db.js
+++ db.js
@@ -1,33 +1,136 @@
 const {MongoClient} = require('mongodb');
 
+const uri = "mongodb://joe:doe@localhost:27017/masterclass_project";
+const client = new MongoClient(uri, {useUnifiedTopology: true});
+const ObjectId = require('mongodb').ObjectID;
+
 async function main() {
-
-    const uri = "mongodb://joe:doe@localhost:27017/masterclass_project";
-
-    const client = new MongoClient(uri, {useUnifiedTopology: true });
-
     try {
-        // Connect to the MongoDB cluster
         await client.connect();
-
-        // Make the appropriate DB calls
-        await listDatabases(client);
-
     } catch (e) {
         console.error(e);
-    } finally {
-        await client.close();
     }
 }
 
-async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
+async function getRestaurantById(restaurant_id) {
+    result = await client.db("masterclass_project").collection("restaurants")
+        .findOne({_id: ObjectId(restaurant_id)});
 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    if (result) {
+        return result;
+    } else {
+        return 'Not finding data ...';
+    }
+}
+
+async function createRestaurant(query) {
+    result = await client.db("masterclass_project").collection("restaurants")
+        .insertOne(
+            {name: query.name, long_coordinates: query.long_coordinates, lat_coordinates: query.lat_coordinates}
+        );
+
+    if (result) {
+        return `Insert new restaurant #${result.insertedId}`;
+    } else {
+        return 'error...';
+    }
+}
+
+async function updateRestaurant(restaurant_id, query) {
+    result = await client.db("masterclass_project").collection("restaurants")
+        .updateOne(
+            {_id: ObjectId(restaurant_id)},
+            {
+                $set: {
+                    name: query.name,
+                    long_coordinates: query.long_coordinates,
+                    lat_coordinates: query.lat_coordinates
+                }
+            }
+        );
+
+    if (result) {
+        return `Edited restaurant #${restaurant_id}`;
+    } else {
+        return 'error...';
+    }
+}
+
+async function deleteRestaurant(restaurant_id) {
+    result = await client.db("masterclass_project").collection("restaurants")
+        .deleteOne({_id: ObjectId(restaurant_id)});
+
+    if (result) {
+        return `Deleted restaurant #${restaurant_id} successful.`;
+    } else {
+        return 'error...';
+    }
+}
+
+async function getAveragePrice() {
+    result = await client.db("masterclass_project").collection("restaurants")
+        .aggregate(
+            [
+                {
+                    $group:
+                        {_id: "", avg_price: {$avg: "$price"}}
+                }
+            ]
+        ).toArray();
+
+    if (result) {
+        return `Average price for all the restaurants is ${result[0].avg_price}`;
+    } else {
+        return 'error...';
+    }
+}
+
+async function getRestaurantRating(restaurant_id) {
+    result = await client.db("masterclass_project").collection("restaurants")
+        .aggregate(
+            [{$project: {_id: "$_id", name: "$name", avg_stars: {$avg: "$reviews"}}}]
+        ).toArray();
+
+    if (result) {
+        return result;
+    } else {
+        return 'error...';
+    }
+}
+
+async function findRestaurantsWithLocation(long_coordinates, lat_coordinates, max_distance) {
+    await client.db("masterclass_project").collection("restaurants").createIndex({"location": "2dsphere"});
+
+    result = await client.db("masterclass_project").collection("restaurants")
+        .find({
+            location: {
+                $near: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [parseFloat(long_coordinates), parseFloat(lat_coordinates)]
+                    },
+                    $maxDistance: parseInt(max_distance ? max_distance : 5000),
+                    $minDistance: 0
+                }
+            }
+        }).toArray();
+
+
+    if (result) {
+        return result;
+    } else {
+        return 'error...';
+    }
 }
 
 main().catch(console.error);
 
-
-module.exports = { main };
+module.exports = {
+    getRestaurantById,
+    createRestaurant,
+    updateRestaurant,
+    deleteRestaurant,
+    getAveragePrice,
+    getRestaurantRating,
+    findRestaurantsWithLocation
+};
